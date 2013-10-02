b6e9b91e30ad05f5dc353260975775baa97de6ae
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-02 09:31:32 -0500

    tweet works

--- app/models/tweet_worker.rb
+++ app/models/tweet_worker.rb
@@ -5,9 +5,33 @@ class TweetWorker
     tweet = Tweet.find(tweet_id)
     user  = tweet.user
 
+    puts "========================================"
+    puts "User: #{user}"
+    puts "User: #{user.inspect}"
+    puts "Tweet: #{tweet}"
+    puts "Tweet: #{tweet.inspect}"
+
     # set up Twitter OAuth client here
     # actually make API call
     # Note: this does not have access to controller/view helpers
     # You'll have to re-initialize everything inside here
+
+    Twitter.configure do |config|
+      config.consumer_key = ENV['TWITTER_KEY']
+      config.consumer_secret = ENV['TWITTER_SECRET']
+    end
+
+
+    twitter_user = Twitter::Client.new(
+      :oauth_token => user.oauth_token,
+      :oauth_token_secret => user.oauth_secret
+    )
+
+    puts "Twitter User: #{twitter_user}"
+    puts "Twitter User: #{twitter_user.inspect}"
+    puts "========================================"
+    
+    twitter_user.update(tweet.text)
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :tweets
 
   def tweet(status)
-    tweet = tweets.create!(:status => status)
+    tweet = tweets.create!(:text => status)
     TweetWorker.perform_async(tweet.id)
   end
 end
